9a5a1d4fb1b82dc427e0d2781abd9167a61c6d0d
MarinaMich <mar7995@yandex.ru>
2024-04-10 23:27:18 +0300

    Создает Функцию для проверки, является ли строка палиндромом

--- js/function.js
+++ js/function.js
@@ -1,1 +1,9 @@
-
+//Функция для проверки, является ли строка палиндромом.
+function getPalindrome(objVerif) {
+  let resultСhanges = '';
+  for(let i = objVerif.length - 1; i >= 0; i--) {
+    resultСhanges += objVerif.at(i);
+  }
+  return objVerif.toLowerCase().replaceAll(' ', '') === resultСhanges.toLowerCase().replaceAll(' ', '');
+}
+getPalindrome('Лёша на полке клопа нашёл ');
