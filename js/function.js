be878c3ed7c5cfe5aecea41ee718fb43f422d56c
MarinaMich <mar7995@yandex.ru>
2024-04-11 13:33:30 +0300

    Добавляет функцию для извлечения из строки чисел

--- js/function.js
+++ js/function.js
@@ -7,3 +7,18 @@ function getPalindrome(objVerif) {
   return objVerif.toLowerCase().replaceAll(' ', '') === resultСhanges.toLowerCase().replaceAll(' ', '');
 }
 getPalindrome('Лёша на полке клопа нашёл ');
+
+//Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+function getNumber(objChoose) {
+  let result = '';
+  let base = objChoose;
+  if (typeof base === 'number') {
+    base = objChoose.toString();
+  }
+  result = Number(base.replace(/([^0-9])/gi, ''));
+  if (typeof result !== 'number') {
+    return NaN;
+  }
+  return result;
+}
+getNumber(1.5);
