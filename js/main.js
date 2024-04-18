c26d1f13789a0d16f3af2f0c69f33d52b90671ea
MarinaMich <mar7995@yandex.ru>
2024-04-18 12:44:58 +0300

    Создает базовые функции, необходимые для дальнейших расчетов

--- js/main.js
+++ js/main.js
@@ -70,3 +70,28 @@ const COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
+
+//Получение случайного целого числа в заданном интервале, включительно
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+//Поиск случайного элемента в переданном массиве.
+
+const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
+const getIdGenerator = () => {
+  let lastCreatedId = 0;
+
+  return () => {
+    lastCreatedId += 1;
+    return lastCreatedId;
+  };
+};
+
+//генерируем id для комментария
+const creatCommentId = getIdGenerator();
