e08a64720a332cb93d5c761369c3c06dad109847
MarinaMich <mar7995@yandex.ru>
2024-05-21 14:55:15 +0300

    Скрывает большое изображение при нажатии на крестик или Esc

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,6 +1,7 @@
 //отрисовка окна с полноразмерным изображением.
 const pictures = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
+const bigPictureCancel = bigPicture.querySelector('#picture-cancel');
 
 const getSelectionPicture = (evt) => {
   if(evt.target.closest('.picture')){
@@ -9,3 +10,14 @@ const getSelectionPicture = (evt) => {
 };
 
 pictures.addEventListener('click', getSelectionPicture);
+
+bigPictureCancel.addEventListener('click', () => {
+  bigPicture.classList.add('hidden');
+});
+
+document.addEventListener('keydown', (evt) => {
+  if(evt.key === 'Escape') {
+    evt.preventDefault();
+    bigPicture.classList.add('hidden');
+  }
+});
