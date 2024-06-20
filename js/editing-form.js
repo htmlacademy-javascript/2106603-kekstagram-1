66d24f97ac1bee88eb8f618e1c92723794e0a993
MarinaMich <mar7995@yandex.ru>
2024-06-20 16:03:57 +0300

    Закрывает окно редактирования

--- js/editing-form.js
+++ js/editing-form.js
@@ -3,6 +3,7 @@ import {isEscapeKey} from './util.js';
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const body = document.body;
 const fieldSelectImg = document.querySelector('#upload-file');
+const imgUploadCancelButton = document.querySelector('#upload-cancel');
 
 const onDocumentKeydown = (evt) => {
   if(isEscapeKey(evt)) {
@@ -10,9 +11,17 @@ const onDocumentKeydown = (evt) => {
     imgUploadOverlay.classList.add('hidden');
   }
 };
+
 const showEditWindow = () => {
   imgUploadOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
 };
+
+const imgUploadCancel = () => {
+  imgUploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
 fieldSelectImg.addEventListener('change', showEditWindow);
+imgUploadCancelButton.addEventListener('click', imgUploadCancel);
