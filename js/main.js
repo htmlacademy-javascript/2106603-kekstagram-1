967d7cc0bb1fce9dba3205749e35d088d07283f8
MarinaMich <mar7995@yandex.ru>
2024-07-01 13:06:16 +0300

    Выводит начальное значение масштаба картинки при открытии окна просмотра

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ import {displayUserPictures} from './gallery.js';
 import {openSelectionPicture} from './big-picture.js';
 import './big-picture.js';
 import './editing-form.js';
+import {resetPreview} from './scaling.js';
 
 const photos = getListPhotos();
 displayUserPictures(photos);
 openSelectionPicture(photos);
+resetPreview();
