581ffaa6965070707406641391dde8d5c6e47ba0
MarinaMich <mar7995@yandex.ru>
2024-07-02 22:34:38 +0300

    Настраивает переключение стилей картинки при выборе фильтра

--- js/effects.js
+++ js/effects.js
@@ -49,13 +49,29 @@ const FILTERS = [
   }
 ];
 
+const NO_FILTER = FILTERS[0];
+let chosenFilter = NO_FILTER;
+
 const effectSlider = document.querySelector('.effect-level__slider');
+const imgPreview = document.querySelector('.img-upload__preview img');
+const filters = document.querySelector('.effects__list');
+const containerEffectSlider = document.querySelector('.img-upload__effect-level');
+const levelEffectSlider = document.querySelector('.effect-level__value');
+
 
 noUiSlider.create(effectSlider, {
   range: {
-    min: 0,
-    max: 100,
+    min: NO_FILTER.min,
+    max: NO_FILTER.max,
   },
-  start: 100,
+  start: NO_FILTER.max,
   connect: 'lower',
+});
+
+filters.addEventListener('change', (evt) => {
+  if(!evt.target.closest('.effects__item')){
+    return;
+  }
+  chosenFilter = FILTERS.find((filter) => filter.name === evt.target.value);
+  imgPreview.className = `effects__preview--${chosenFilter.name}`;
 });
