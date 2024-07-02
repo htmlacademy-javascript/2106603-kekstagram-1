abdfd67af2438891605e1cc339fc6dfe32863a76
MarinaMich <mar7995@yandex.ru>
2024-07-02 20:00:17 +0300

    Добавляет массив фильтров к картинке

--- js/effects.js
+++ js/effects.js
@@ -1,3 +1,54 @@
+const FILTERS = [
+  {
+    name: 'none',
+    filter: 'none',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '',
+  },
+  {
+    name: 'chrome',
+    filter: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  {
+    name: 'sepia',
+    filter: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  {
+    name: 'marvin',
+    filter: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%',
+  },
+  {
+    name: 'phobos',
+    filter: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    unit: 'px',
+  },
+  {
+    name: 'heat',
+    filter: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    unit: '',
+  }
+];
+
 const effectSlider = document.querySelector('.effect-level__slider');
 
 noUiSlider.create(effectSlider, {
