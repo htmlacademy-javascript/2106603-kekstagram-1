0933a3d3498e054f19545c64fc96ebc5b893f38b
MarinaMich <mar7995@yandex.ru>
2024-07-12 12:17:46 +0300

    Подключает получение данных с сервера

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-import {getListPhotos} from './data.js';
 import {displayUserPictures} from './gallery.js';
 import {openSelectionPicture} from './big-picture.js';
 import './editing-form.js';
 import './effects.js';
 
-const photos = getListPhotos();
-displayUserPictures(photos);
-openSelectionPicture(photos);
+//const photos = getListPhotos();
+//displayUserPictures(photos);
+//openSelectionPicture(photos);
+fetch('https://28.javascript.htmlacademy.pro/kekstagram/data')
+  .then((response) => response.json())
+  .then((picture) => {
+    displayUserPictures(picture);
+    openSelectionPicture(picture);
+  });
