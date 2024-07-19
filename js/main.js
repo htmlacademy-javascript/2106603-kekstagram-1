2b5a54f96d6694fdd038ba72cc42af725ed4d3d7
MarinaMich <mar7995@yandex.ru>
2024-07-19 23:25:22 +0300

    Добавляет вывод сообщение о проблеме загрузки данных

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ import {openSelectionPicture} from './big-picture.js';
 import {setImgFormSubmit, imgUploadCancel} from './editing-form.js';
 import './effects.js';
 import {getData} from './api.js';
+import {showAlert} from './util.js';
 
 getData()
   .then((picture) => {
     displayUserPictures(picture);
     openSelectionPicture(picture);
-  });
+  })
+  .catch((err) => showAlert(err.message));
 
 setImgFormSubmit(imgUploadCancel);
