82d1ade2e1b307c5ff3f7c31f38fc846de2da5f2
MarinaMich <mar7995@yandex.ru>
2024-08-13 22:47:46 +0300

    Выносит переменную j из цикла

--- js/util.js
+++ js/util.js
@@ -44,8 +44,9 @@ const showAlert = (message) => {
 };
 
 const randomSelection = (array) => {
+  let j;
   for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
+    j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
 };
